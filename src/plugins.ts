b6b025ccace03994dca101c57c9afe5621830530
Matteo Di Rollo <matteo.dirollo@icloud.com>
2025-05-03 18:19:41 +0200

    isGlobal true

--- src/plugins.ts
+++ src/plugins.ts
@@ -9,34 +9,35 @@ import { paymentCanceled } from "./webhooks/payment-canceled";
 import { paymentSucceeded } from "./webhooks/payment-succeeded";
 
 export const plugins: Plugin[] = [
-    stripePlugin({
-        isTestKey: Boolean(process.env.NEXT_PUBLIC_STRIPE_IS_TEST_KEY),
-        logs: true,
-        rest: false,
-        stripeSecretKey: process.env.STRIPE_SECRET_KEY || "",
-        stripeWebhooksEndpointSecret:
-            process.env.STRIPE_WEBHOOKS_SIGNING_SECRET,
-        webhooks: {
-            "payment_intent.canceled": paymentCanceled,
-            "payment_intent.succeeded": paymentSucceeded,
-        },
-    }),
-    cjPlugin({
-        cjApiKey: process.env.CJ_PASSWORD || "",
-        cjEmailAddress: process.env.CJ_EMAIL_ADDRESS || "",
-        cjRefreshToken: process.env.CJ_REFRESH_TOKEN,
-    }),
-    storePlugin({}),
-    importExportPlugin({
-        collections: ["products", "orders"],
-        disableJobsQueue: true,
-        importCollections: [
-            {
-                collectionSlug: "products",
-            },
-            {
-                collectionSlug: "orders",
-            },
-        ],
-    }),
+  stripePlugin({
+    isTestKey: Boolean(process.env.NEXT_PUBLIC_STRIPE_IS_TEST_KEY),
+    logs: true,
+    rest: false,
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY || "",
+    stripeWebhooksEndpointSecret: process.env.STRIPE_WEBHOOKS_SIGNING_SECRET,
+    webhooks: {
+      "payment_intent.canceled": paymentCanceled,
+      "payment_intent.succeeded": paymentSucceeded,
+    },
+  }),
+  cjPlugin({
+    cjApiKey: process.env.CJ_PASSWORD || "",
+    cjEmailAddress: process.env.CJ_EMAIL_ADDRESS || "",
+    cjRefreshToken: process.env.CJ_REFRESH_TOKEN,
+    isGlobal: true,
+    // collectionOverrides: {},
+  }),
+  storePlugin({}),
+  importExportPlugin({
+    collections: ["products", "orders"],
+    disableJobsQueue: true,
+    importCollections: [
+      {
+        collectionSlug: "products",
+      },
+      {
+        collectionSlug: "orders",
+      },
+    ],
+  }),
 ];
