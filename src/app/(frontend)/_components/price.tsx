58c38ae6c197333637e61abbbfe101e725bc1b06
Matteo Di Rollo <matteo.dirollo@icloud.com>
2025-05-07 16:53:19 +0200

    Stripe shipping addresses

--- src/app/(frontend)/_components/price.tsx
+++ src/app/(frontend)/_components/price.tsx
@@ -3,38 +3,38 @@ import { clx, Text } from "@medusajs/ui";
 import { convertToLocale } from "../_util/money";
 
 export default function PreviewPrice({
-    currency,
-    originalPrice,
-    price,
+  currency,
+  originalPrice,
+  price,
 }: {
-    currency?: string;
-    originalPrice?: any;
-    price: any;
+  currency?: string;
+  originalPrice?: any;
+  price: any;
 }) {
-    return (
-        <>
-            {originalPrice && (
-                <Text
-                    className="line-through text-ui-fg-muted"
-                    data-testid="original-price"
-                >
-                    {convertToLocale({
-                        amount: originalPrice,
-                        currency_code: currency || "USD",
-                    })}
-                </Text>
-            )}
-            <Text
-                className={clx("text-ui-fg-muted", {
-                    "text-ui-fg-interactive": originalPrice,
-                })}
-                data-testid="price"
-            >
-                {convertToLocale({
-                    amount: price,
-                    currency_code: currency || "USD",
-                })}
-            </Text>
-        </>
-    );
+  return (
+    <>
+      {originalPrice && (
+        <Text
+          className="line-through text-ui-fg-muted"
+          data-testid="original-price"
+        >
+          {convertToLocale({
+            amount: originalPrice,
+            currency_code: currency || "EUR",
+          })}
+        </Text>
+      )}
+      <Text
+        className={clx("text-ui-fg-muted", {
+          "text-ui-fg-interactive": originalPrice,
+        })}
+        data-testid="price"
+      >
+        {convertToLocale({
+          amount: price,
+          currency_code: currency || "EUR",
+        })}
+      </Text>
+    </>
+  );
 }
