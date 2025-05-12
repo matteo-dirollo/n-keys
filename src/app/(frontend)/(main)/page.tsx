9f0027de56343684013629ac94fa55c90109f82c
Matteo Di Rollo <matteo.dirollo@icloud.com>
2025-05-12 11:13:30 +0200

    Title and description

--- src/app/(frontend)/(main)/page.tsx
+++ src/app/(frontend)/(main)/page.tsx
@@ -7,35 +7,35 @@ import FeaturedProducts from "../_components/featured-products";
 import Hero from "../_components/hero";
 
 export const metadata: Metadata = {
-    description:
-        "A performant frontend ecommerce starter template with Payload CMS and Shopnex.",
-    title: "Shopnex Ecommerce Starter Template",
+  description:
+    "Your one-stop shop for all things e-commerce. From product management to order fulfillment, we've got you covered.",
+  title: "N-KEYS | E-commerce Platform",
 };
 
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-    const payload = await getPayload({ config });
+  const payload = await getPayload({ config });
 
-    const heroSection = await payload.findGlobal({
-        slug: "hero-section",
-    });
-    const hero = heroSection.type?.find((f) => f.blockType === "hero");
+  const heroSection = await payload.findGlobal({
+    slug: "hero-section",
+  });
+  const hero = heroSection.type?.find((f) => f.blockType === "hero");
 
-    const featuredCollections = await payload.find({
-        collection: "collections",
-        limit: 3,
-        sort: "createdAt",
-    });
+  const featuredCollections = await payload.find({
+    collection: "collections",
+    limit: 3,
+    sort: "createdAt",
+  });
 
-    return (
-        <>
-            <Hero hero={hero as any} />
-            <div className="py-12">
-                <ul className="flex flex-col gap-x-6">
-                    <FeaturedProducts collections={featuredCollections.docs} />
-                </ul>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Hero hero={hero as any} />
+      <div className="py-12">
+        <ul className="flex flex-col gap-x-6">
+          <FeaturedProducts collections={featuredCollections.docs} />
+        </ul>
+      </div>
+    </>
+  );
 }
